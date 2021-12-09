7563d54579fdf7b0141fc2ddaadd6cf7279ae2b0
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2021-12-10 01:17:44 +0600

    Importing Own Files-One module to another

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,23 @@
+/*---------Importing Own Files-------*/
+
+/*
+const add = require('./utils');
+const summation = add(4,-2);
+console.log(summation);
+*/
+
+
+/*
+const name = require('./utils');
+console.log(name);
+*/
+
+/*
 const fs = require('fs');
 const getNotes = require('./notes');
-
 const msg = getNotes();
 console.log(msg);
-
 fs.writeFileSync('notes.txt', 'My name is Tahmid.');
-
 fs.appendFileSync('notes.txt','I am 25 years old');
-
+*/
 
---
+++ notes-app/utils.js
@@ -0,0 +1,7 @@
+const name = 'Tahmid Hasan';
+
+const sum = (a,b)=>{
+    return a+b
+}
+
+module.exports = sum;
