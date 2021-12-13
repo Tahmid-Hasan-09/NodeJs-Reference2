f9588a29d4817533d537fdbb34b739c743fd7573
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2021-12-13 20:23:29 +0600

    Take Input & Manipulate From Command Line Arguments Via yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -36,3 +36,54 @@ fs.appendFileSync('notes.txt','I am 25 years old');
 // } else if (command === 'remove') {
 //     console.log('Removing note!')
 // }
+
+/*---------Manipulate Command Line Arguments By 'yargs' package-------*/
+const yargs = require('yargs');
+yargs.version('1.1.1');
+//create add comand
+yargs.command({
+    command:'add',
+    describe:'add a note',
+    builder:{
+        title:{
+            describle:'Note title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Note body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log(`Title:${argv.title}`);
+        console.log(`Body:${argv.body}`);
+    }
+})
+//remove add comand
+yargs.command({
+    command:'remove',
+    describe:'remove a note',
+    handler:function(){
+        console.log('Removing A Note');
+    }
+})
+//create List comand
+yargs.command({
+    command:'list',
+    describe:'List Notes',
+    handler:function(){
+        console.log('Listing All notes');
+    }
+})
+//create read comand
+yargs.command({
+    command:'read',
+    describe:'read a note',
+    handler:function(){
+        console.log('Reading A Note');
+    }
+})
+yargs.parse();
+//console.log(yargs.argv)
