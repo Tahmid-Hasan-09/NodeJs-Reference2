57ca12a6fe89e75a3a4a181d92ed5e42fbd8b8fc
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2021-12-15 08:29:24 +0600

    JSON,Obj,BufferData Conversion to One Another

---
+++ playground/1-json.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+//Sample Obj Create
+const book = {
+    title:'Ego is the Enemy',
+    author:'Ryan Holiday'
+}
+
+//Convert Obj to JSON
+const bookJSON = JSON.stringify(book);
+
+//Write To json file using fs package
+fs.writeFileSync('1-json.json',bookJSON);
+
+//Read json file using fs package
+const dataBuffer = fs.readFileSync('1-json.json');
+
+//Convert Buffer Data To Original Json Data
+const dataJson = dataBuffer.toString();
+
+//Convert JSON to Obj
+const data = JSON.parse(dataJson)
+
+// console.log(dataBuffer)
+// console.log(dataJson)
+console.log(data.title)
