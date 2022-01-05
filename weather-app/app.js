32239b20623bb3b208eb4b46af5059ea7d395c60
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-06 05:47:54 +0600

    Comment Out

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,13 @@
 //Asynchronous NodeJs Reference Example by Order of Execution
-console.log('Starting')
+// console.log('Starting')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+// setTimeout(() => {
+//     console.log('2 Second Timer')
+// }, 2000)
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
+// setTimeout(() => {
+//     console.log('0 Second Timer')
+// }, 0)
 
-console.log('Stopping')
+// console.log('Stopping')
+
