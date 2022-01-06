8301c63cf7192667f99b1649d2da8c0b9ed39bb8
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-06 14:51:33 +0600

    Insert Basic Functionalities to weather API

--- weather-app/app.js
+++ weather-app/app.js
@@ -13,8 +13,11 @@
 
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596'
+const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
 
-request({ url: url, json:true }, (error, response) => {
-    
+request({
+    url: url,
+    json: true
+}, (error, response) => {
+    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degress out.It feels like ${response.body.current.feelslike} degrees out`)
 })
