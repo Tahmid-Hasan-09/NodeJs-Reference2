411b3123a005c1e4c44ebb6ec8537326c5bd8f24
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-06 22:57:46 +0600

    Using mapbox geocoding API

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,12 +12,22 @@
 // console.log('Stopping')
 
 const request = require('request')
-
-const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
+//Request weatherstack API
+const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f';
 
 request({
     url: url,
     json: true
 }, (error, response) => {
     console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degress out.It feels like ${response.body.current.feelslike} degrees out`)
+})
+
+//Request mapbox geocoding API
+const uri = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGFobWlkLWhhc2FuIiwiYSI6ImNreTJ0b3dzcjBqeTkyb255dWxidHgxY3QifQ.aWPyDh9Z8VsTaESckyIhCA';
+
+request({
+    uri:uri,
+    json:true
+},(error,response)=>{
+    console.log(`${response.body.features[0].place_name}, Longitude:${response.body.features[0].center[0]},Latitude:${response.body.features[0].center[1]}`)
 })
