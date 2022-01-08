55d8658e5f9505106b54034cf34d48b89cc13600
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-08 06:32:24 +0600

    Replacing with ES6 object destructuring & property shorthand

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,21 +14,39 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 // Request mapbox geocoding API
-if (process.argv[2] === undefined) {
+if(process.argv[2] === undefined){
     console.log('Please Enter a Location');
-} else {
+}else{
     const location = process.argv[2];
-    geocode(location, (error, data) => {
+    // geocode(location, (error, data) => {
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+    //     console.log('Geocode', data);
+    //     //Request weatherstack API for forecast    
+    //     forecast(data.latitude, data.longitude, (error, forecastData) => {
+    //         if (error) {
+    //             return console.log(error)
+    //         }
+    //         console.log(`${data.place} : ${forecastData}`)
+    //     })
+    // })
+    //Another Version with Replacing with object destructuring
+    geocode(location, (error, {
+        longitude,
+        latitude,
+        place
+    } = {}) => {
         if (error) {
             return console.log(error);
         }
-        console.log('Geocode', data);
         //Request weatherstack API for forecast    
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
             }
-            console.log(`${data.place} : ${forecastData}`)
+            console.log(`${place} : ${forecastData}`)
         })
     })
 }
+
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,24 +1,42 @@
 const request = require('request');
 
-const geocode = (address,callback)=>{
+const geocode = (address, callback) => {
     const uri = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoidGFobWlkLWhhc2FuIiwiYSI6ImNreTJ0b3dzcjBqeTkyb255dWxidHgxY3QifQ.aWPyDh9Z8VsTaESckyIhCA`;
 
+    // request({
+    //     uri: uri,
+    //     json: true
+    // }, (error, response) => {
+    //     if (error) {
+    //         callback('Unable to connect to weather service!', undefined);
+    //     } else if (response.body.features.length === 0) {
+    //         callback('Unable to find location', undefined);
+    //     } else {
+    //         callback(undefined, {
+    //             longitude: response.body.features[0].center[0],
+    //             latitude: response.body.features[0].center[1],
+    //             place: response.body.features[0].place_name
+    //         });
+    //     }
+    // })
+    //Another Version with Replacing with object destructuring & Object property Shorthand
     request({
-        uri:uri,
-        json:true
-    },(error,response)=>{
-        if(error){
-            callback('Unable to connect to weather service!',undefined);
-        }else if(response.body.features.length === 0){
-            callback('Unable to find location',undefined);
-        }else{  
-            callback(undefined,{
-                longitude:response.body.features[0].center[0],
-                latitude:response.body.features[0].center[1],
-                place:response.body.features[0].place_name
-            });    
-        }    
+        uri,
+        json: true
+    }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.features.length === 0) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, {
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                place: body.features[0].place_name
+            });
+        }
     })
 }
+
 
 module.exports = geocode
