ead28e78fb608406fc49e25eede6b5ec041347ff
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-09 03:32:01 +0600

    Commenting Out

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const app = express()
 
+/*
 app.get('', (req, res) => {
     res.send('Hello express!')
 })
@@ -17,6 +18,7 @@ app.get('/about', (req, res) => {
 app.get('/weather', (req, res) => {
     res.send('Your weather')
 })
+*/
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
