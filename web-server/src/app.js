5349009eeadd22b4c87136da2e73b23b5b04c63d
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-09 06:20:39 +0600

    Set up public directory path for express server use

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,13 @@
+const path = require('path')
 const express = require('express')
-
 const app = express()
+
+//Node Provided Variable
+console.log(__dirname);
+console.log(__filename);
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath));
 
 /*
 app.get('', (req, res) => {
@@ -20,6 +27,7 @@ app.get('/weather', (req, res) => {
 })
 */
 
+/*
 app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
 })
@@ -35,6 +43,8 @@ app.get('/help', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('<h1>About</h1>')
 })
+
+*/
 
 app.get('/weather', (req, res) => {
     res.send({
