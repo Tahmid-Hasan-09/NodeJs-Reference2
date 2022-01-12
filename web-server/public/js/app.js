1e6ad6d623b9239c51ab820d61f0e57e82995a02
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-12 07:02:42 +0600

    HTML & CSS Manipulation with Javascript DOM Manipulation

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -9,17 +9,23 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
 weatherForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault();//Prevent Browser From Auto -Refresh after the event happened
     const location = search.value;
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
-        response.json().then((data) => {
+        response.json().then((data) => { 
             // console.log(data)
             if (data.error) {
-                console.log(data.error)
+                message1.textContent = data.error
             } else {
-                console.log(data.location) 
-                console.log(data.forecast)
+                message1.textContent = data.location
+                message2.textContent = data.forecast
             }
         })
     })
