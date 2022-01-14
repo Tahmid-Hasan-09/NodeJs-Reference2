794c93328970f460f1b10abb486f2be8558e9c16
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-14 12:00:04 +0600

    Database Inserting technique with mongodb native driver

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,4 +12,43 @@ MongoClient.connect(connectionURL, {
 
     const db = client.db(databaseName);
     const collection = db.collection('users');
+
+    // collection.insertOne({
+    //     name:'Tahmid Hasan',
+    //     age : 27
+    // },(error,newUser)=>{
+    //     if(error){
+    //         return console.log('Unable to connect to database!');
+    //     }
+    //     console.log(newUser)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result)
+    // })
+
+    db.collection('tasks').insertMany([{
+        description: 'Man is Mortal',
+        completed: true
+    }, {
+        description: 'The job must have been completed',
+        completed: false
+    }], (error, newlyInserted) => {
+        if (error) {
+            return console.log('Unable to insert documents!');
+        }
+        console.log(newlyInserted)
+    })
 })
