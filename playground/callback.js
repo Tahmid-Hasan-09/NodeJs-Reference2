6a00b8a1c109988f7dd050a9f485c2ee59bf0fcc
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-16 05:49:01 +0600

    Commenting Out

--- playground/callback.js
+++ playground/callback.js
@@ -1,34 +1,34 @@
-setTimeout(() => {
-    console.log('Two seconds are up')
-}, 2000)
+// setTimeout(() => {
+//     console.log('Two seconds are up')
+// }, 2000)
 
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
 
-const geocode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
 
-        callback(data)
-    }, 2000)
-}
+//         callback(data)
+//     }, 2000)
+// }
 
-geocode('Philadelphia', (data) => {
-    console.log(data)
-})
+// geocode('Philadelphia', (data) => {
+//     console.log(data)
+// })
 
-const add = (num1,num2,callback)=>{
-    setTimeout(()=>{
-        let sum = num1+num2;
-        callback(sum);
-    },2000)
-}
+// const add = (num1,num2,callback)=>{
+//     setTimeout(()=>{
+//         let sum = num1+num2;
+//         callback(sum);
+//     },2000)
+// }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
-})
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
