1c278fcf039568e9485d4096ad427954e938b806
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-18 21:40:40 +0600

    Read User & Task From Database

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,12 +8,60 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/user',(req,res)=>{
+app.post('/users',(req,res)=>{
     const user = new User(req.body);
     user.save().then((user)=>{
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error)=>{
         res.status(400).send(error)
+    })
+})
+
+app.get('/users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((error)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id;
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error)=>{
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks',(req,res)=>{
+    const task = new Task(req.body);
+    task.save().then((task)=>{
+        res.status(201).send(task)
+    }).catch((error)=>{
+        res.status(400).send(error)
+    })
+})
+
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((error)=>{
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id',(req,res)=>{
+    Task.findById(req.params.id).then((task)=>{
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((error)=>{
+        res.status(500).send(error)
     })
 })
 
