ec323c21406d152ccbb81c79827d95c560aaec65
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-19 04:42:31 +0600

    Playing with promise based async-await function

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,13 +1,25 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('61e697a3b7b0a58251582318',{
-    age : 22
-}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:22})
-}).then((result)=>{
-    console.log(result)
-}).catch((e)=>{
+// User.findByIdAndUpdate('61e697a3b7b0a58251582318',{
+//     age : 22
+// }).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:22})
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age:age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('61e697a3b7b0a58251582318', 20).then((count) => {
+    console.log(count)
+}).catch((e) => {
     console.log(e)
 })
--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -1,13 +1,25 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('61e6cf6534d4a7bd4b987fd1').then((task)=>{
-    console.log(task)
-    return Task.countDocuments({
-        completed : false
-    })
-}).then((result)=>{
-    console.log(result)
+// Task.findByIdAndDelete('61e6cf6534d4a7bd4b987fd1').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({
+//         completed : false
+//     })
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+const deleteTaskAndCount = async (id)=>{
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed:false});
+    return count;
+}
+
+deleteTaskAndCount('61e6a1692475d6d665680bef').then((count)=>{
+    console.log(count);
 }).catch((error)=>{
-    console.log(error)
+    console.log(error);
 })
