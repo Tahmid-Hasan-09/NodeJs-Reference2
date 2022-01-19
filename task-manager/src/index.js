bba44bd54b38a27dc9c1093a1f1a1ece9e4402ea
Tahmid Hasan <tahmidhasan.170109@s.pust.ac.bd>
2022-01-19 18:32:47 +0600

    Updating & Deleting Routes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -73,6 +73,42 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 // app.post('/tasks',(req,res)=>{
 //     const task = new Task(req.body);
 //     task.save().then((task)=>{
@@ -129,6 +165,37 @@ app.get('/tasks/:id',async (req,res)=>{
         res.status(201).send(task)
     }catch(error){
         res.status(500).send(error)
+    }
+})
+
+app.patch('/tasks/:id',async (req,res)=>{
+    const updates = Object.keys(req.body);//convert objects to array(object keys array)
+    const allowedUpdates = ['description','completed'];
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update));
+    if(!isValidOperation){
+        return res.status(400).send({error:'Invalid Updates!'})
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body,{ new:true, runValidators:true });
+        if(!task){
+            return res.status(404).send();
+        }
+        res.status(202).send(task);
+    }catch(error){
+        res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id',async (req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    }catch(error){
+        res.status(500).send();
     }
 })
 
